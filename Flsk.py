6d24a942e7f7141609029ed28f256603c7637775
nictit <87221489+nictit@users.noreply.github.com>
2022-09-12 09:15:45 +0300

    work correctly

--- Flsk.py
+++ Flsk.py
@@ -13,10 +13,12 @@ def index():
     print(str(chat_id), str(user_name), str(msg_text))
     reg = sub(r'[^\w\s]', '', msg_text)
     planesBackEnd.addPlaneToTrack(str(reg))
+    return 'its ok'
 
 def main():
     TGapi.deleteWH()
     print('setting WH')
-    TGapi.setWH('https://fortesty.herokuapp.com')
+    #TGapi.setWH('https://fortesty.herokuapp.com')
+    print(TGapi.setWH('https://3ee0-87-226-251-234.ngrok.io'))
     print('WH set')
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
