489683a3d1e05077af016611979b1b98e3b5435d
nictit <87221489+nictit@users.noreply.github.com>
2022-09-21 05:36:09 +0300

    -

--- Flsk.py
+++ Flsk.py
@@ -35,7 +35,7 @@ def index():
 
 def main():
     TGapi.deleteWH()
-    #print(TGapi.setWH('https://whereismyairbot.herokuapp.com'))
-    print(TGapi.setWH('https://6ae7-87-226-251-234.ngrok.io'))
+    print(TGapi.setWH('https://whereismyairbot.herokuapp.com'))
+    #print(TGapi.setWH('https://6ae7-87-226-251-234.ngrok.io'))
     print('WH set')
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
